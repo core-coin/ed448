4ee6089f1533a806b6b346a7cf04f7c5161314fb
Dmitry Redkin <dmitry@wallit.eu>
2021-10-07 01:42:42 +0300

    New Key Scheme

--- goldilocks.go
+++ goldilocks.go
@@ -24,6 +24,26 @@ func PointByPrivate(p PrivateKey) Point {
 	return h
 }
 
+// SLICES TO KEYS
+
+func BytesToPublicKey(key []byte) (pk PublicKey) {
+	if len(key) != len(PublicKey{}) {
+		return PublicKey{}
+	}
+	copy(pk[:], key)
+	return
+}
+
+func BytesToPrivateKey(key []byte) (pk PrivateKey) {
+	if len(key) != len(PrivateKey{}) {
+		return PrivateKey{}
+	}
+	copy(pk[:], key)
+	return
+}
+
+// DIFFIE-HELLMAN SHARED SECRET
+
 func EdPrivateKeyToX448(edKey PrivateKey) [56]byte {
 	x := [56]byte{}
 	sha3.ShakeSum256(x[:], edKey[:])
@@ -35,7 +55,14 @@ func EdPublicKeyToX448(edKey PublicKey) [56]byte {
 }
 
 func Ed448DeriveSecret(pubkey PublicKey, privkey PrivateKey) [56]byte {
-	xpriv := EdPrivateKeyToX448(privkey)
+
+	var xpriv = [56]byte{0}
+	if privkey[57-1]&0x80 == 0x00 {
+		xpriv = EdPrivateKeyToX448(privkey)
+	} else {
+		copy(xpriv[:], privkey[0:56])
+	}
+
 	xpub := EdPublicKeyToX448(pubkey)
 	a, b := x448ScalarMul(xpub[:], xpriv[:])
 	if !b {
@@ -44,14 +71,53 @@ func Ed448DeriveSecret(pubkey PublicKey, privkey PrivateKey) [56]byte {
 	return a
 }
 
-func Ed448DerivePublicKey(privkey PrivateKey) PublicKey {
+// PRIVATE, SECRET AND PUBLIC
+
+func PrivateToSecret(pk PrivateKey) PrivateKey {
+
+	var sk PrivateKey
+	sha3.ShakeSum256(sk[:], pk[:])
+	return sk
+}
+
+func SecretToPublic(sk PrivateKey) PublicKey {
+
+	var s1 PrivateKey
+	copy(s1[:], sk[:])
+	clamp(s1[:])
+	r := NewScalar(s1[:])
+	r.Halve(r)
+	r.Halve(r)
+	h := PrecomputedScalarMul(r)
+
+	var pub PublicKey
+	p := h.EdDSAEncode()
+	copy(pub[:], p[:])
+
+	Zero := PrivateKey{0}
+	copy(s1[:], Zero[:])
+	return pub
+}
+
+func PrivateToPublic(privkey PrivateKey) PublicKey {
 	var pub PublicKey
 	p := PointByPrivate(privkey).EdDSAEncode()
 	copy(pub[:], p[:])
 	return pub
 }
 
-func Ed448Sign(privkey PrivateKey, pubkey PublicKey, message, context []byte, prehashed bool) [114]byte {
+func Ed448DerivePublicKey(privkey PrivateKey) PublicKey {
+	if privkey[57-1]&0x80 == 0x00 {
+		return PrivateToPublic(privkey)
+	} else {
+		return SecretToPublic(privkey)
+	}
+}
+
+
+// CREATE SIGNATURE
+
+func SignWithPrivate(privkey PrivateKey, pubkey PublicKey, message, context []byte, prehashed bool) [114]byte {
 	if len(context) != 0 {
 		panic("Context is not supported!")
 	}
@@ -65,6 +131,74 @@ func Ed448Sign(privkey PrivateKey, pubkey PublicKey, message, context []byte, pr
 	}
 	return DSASign(privkey, p, message)
 }
+
+func SignSecretAndNonce(secret, n PrivateKey, pubkey PublicKey, msg []byte) [114]byte {
+
+	var s1 PrivateKey
+	copy(s1[:], secret[:])
+	clamp(s1[:])
+
+	pub := NewPoint([16]uint32{}, [16]uint32{}, [16]uint32{}, [16]uint32{})
+
+	if !pub.EdDSADecode(pubkey[:]) {
+		panic("Point is not on the curve!")
+	}
+
+	sec := NewScalar(s1[:])
+	seed := n[:]
+
+	nonce := make([]byte, 114)
+	hashWithDom(nonce, append(seed, msg...))
+	nonceScalar := NewScalar(nonce[:])
+	nonceScalar2 := NewScalar()
+	nonceScalar2.Halve(nonceScalar)
+	nonceScalar2.Halve(nonceScalar2)
+	noncePoint := PrecomputedScalarMul(nonceScalar2).EdDSAEncode()
+
+	challenge := make([]byte, 114)
+	hashWithDom(challenge, append(append(noncePoint, pub.EdDSAEncode()...), msg...))
+
+	challengeScalar := NewScalar(challenge)
+	challengeScalar.Mul(challengeScalar, sec)
+	challengeScalar.Add(challengeScalar, nonceScalar)
+
+	var sig [114]byte
+	copy(sig[:], noncePoint)
+	copy(sig[57:], challengeScalar.Encode())
+
+	Zero := PrivateKey{0}
+	copy(s1[:], Zero[:])
+
+	return sig
+}
+
+
+func Ed448Sign(privkey PrivateKey, pubkey PublicKey, message, context []byte, prehashed bool) [114]byte {
+
+	if privkey[57-1]&0x80 == 0x00 {
+		return SignWithPrivate(privkey, pubkey, message, context, prehashed)
+	} else {
+		if len(context) != 0 {
+			panic("Context is not supported!")
+		}
+		if prehashed {
+			panic("Prehashing is not supported!")
+		}
+		var sk PrivateKey
+		copy(sk[:], privkey[:])
+		clamp(sk[:])
+		
+		sig := SignSecretAndNonce(sk, sk, pubkey, message)
+
+		// Erasing temporary values of private keys
+		var sZero = PrivateKey{0}
+		copy(sk[:], sZero[:])
+		
+		return sig
+	}
+}
+
+// VERIFY SIGNATURE
 
 func Ed448Verify(pubkey PublicKey, signature, message, context []byte, prehashed bool) bool {
 	if len(context) != 0 {
@@ -84,6 +218,33 @@ func Ed448Verify(pubkey PublicKey, signature, message, context []byte, prehashed
 	return DSAVerify(sig, p, message)
 }
 
+// ADD TWO PUBLIC KEYS (FOR HDwallet)
+
+func AddTwoPublic(pub1 PublicKey, pub2 PublicKey) PublicKey {
+
+	var pub PublicKey
+	p := NewPoint([16]uint32{}, [16]uint32{}, [16]uint32{}, [16]uint32{})
+	p1 := NewPoint([16]uint32{}, [16]uint32{}, [16]uint32{}, [16]uint32{})
+	p2 := NewPoint([16]uint32{}, [16]uint32{}, [16]uint32{}, [16]uint32{})
+
+	if !p1.EdDSADecode(pub1[:]) {
+		panic("Point is not on the curve!")
+	}
+	if !p2.EdDSADecode(pub2[:]) {
+		panic("Point is not on the curve!")
+	}
+	p.Add(p1, p2)
+
+	r := p.EdDSAEncode()
+
+	copy(pub[:], r[:])
+
+	return pub
+}
+
+
+// GENERATE PRIVATE KEY
+
 func Ed448GenerateKey(reader io.Reader) (PrivateKey, error) {
 	key := new(PrivateKey)
 	n, err := io.ReadFull(reader, key[:])
@@ -92,21 +253,6 @@ func Ed448GenerateKey(reader io.Reader) (PrivateKey, error) {
 	} else if n != 57 {
 		return PrivateKey{}, fmt.Errorf("not 57 random bytes")
 	}
+	key[56] &= 0x7f
 	return *key, nil
-}
-
-func BytesToPublicKey(key []byte) (pk PublicKey) {
-	if len(key) != len(PublicKey{}) {
-		return PublicKey{}
-	}
-	copy(pk[:], key)
-	return
-}
-
-func BytesToPrivateKey(key []byte) (pk PrivateKey) {
-	if len(key) != len(PrivateKey{}) {
-		return PrivateKey{}
-	}
-	copy(pk[:], key)
-	return
 }
--- goldilocks_test.go
+++ goldilocks_test.go
@@ -3,200 +3,278 @@ package ed448
 import (
 	"bytes"
 	"testing"
+	"encoding/hex"
+	crand "crypto/rand"
 )
 
 func TestEdPublicKeyToX448(t *testing.T) {
-	// b93a28627cfa29fedb03c21aac0faa1ea0ba84c10cefa07c938f2e0adbf996f02c8d00e39695dfb6a0636c8bcb21645b06a869dfbbb489ef00
-	edKey := PublicKey{
-		0xb9, 0x3a, 0x28, 0x62, 0x7c, 0xfa, 0x29, 0xfe,
-		0xdb, 0x03, 0xc2, 0x1a, 0xac, 0x0f, 0xaa, 0x1e,
-		0xa0, 0xba, 0x84, 0xc1, 0x0c, 0xef, 0xa0, 0x7c,
-		0x93, 0x8f, 0x2e, 0x0a, 0xdb, 0xf9, 0x96, 0xf0,
-		0x2c, 0x8d, 0x00, 0xe3, 0x96, 0x95, 0xdf, 0xb6,
-		0xa0, 0x63, 0x6c, 0x8b, 0xcb, 0x21, 0x64, 0x5b,
-		0x06, 0xa8, 0x69, 0xdf, 0xbb, 0xb4, 0x89, 0xef,
-		0x00,
-	}
 
-	// 163af30230e62cbf36fd8f4713f2204d78fa8f94f79adfe4f49ed1075d12b3a725a5e5c0564faa6445900b4d166b89b76f2db5c374411129
-	x448Key := [56]byte{
-		0x16, 0x3a, 0xf3, 0x02, 0x30, 0xe6, 0x2c, 0xbf,
-		0x36, 0xfd, 0x8f, 0x47, 0x13, 0xf2, 0x20, 0x4d,
-		0x78, 0xfa, 0x8f, 0x94, 0xf7, 0x9a, 0xdf, 0xe4,
-		0xf4, 0x9e, 0xd1, 0x07, 0x5d, 0x12, 0xb3, 0xa7,
-		0x25, 0xa5, 0xe5, 0xc0, 0x56, 0x4f, 0xaa, 0x64,
-		0x45, 0x90, 0x0b, 0x4d, 0x16, 0x6b, 0x89, 0xb7,
-		0x6f, 0x2d, 0xb5, 0xc3, 0x74, 0x41, 0x11, 0x29,
-	}
-	generatedKey := EdPublicKeyToX448(edKey)
+	p1, _ := hex.DecodeString("b93a28627cfa29fedb03c21aac0faa1ea0ba84c10cefa07c938f2e0adbf996f02c8d00e39695dfb6a0636c8bcb21645b06a869dfbbb489ef00")
+	var public PublicKey
+	copy(public[:], p1[:])
 
+	p2, _ := hex.DecodeString("163af30230e62cbf36fd8f4713f2204d78fa8f94f79adfe4f49ed1075d12b3a725a5e5c0564faa6445900b4d166b89b76f2db5c374411129")
+	var x448Key [56]byte
+	copy(x448Key[:], p2[:])
+
+	generatedKey := EdPublicKeyToX448(public)
+	
 	if bytes.Compare(x448Key[:], generatedKey[:]) != 0 {
-		t.Errorf("converted x448 key must be %x, but it is %x", generatedKey, x448Key)
+		t.Errorf("x448 key must be: %x\n But it is: %x", x448Key, generatedKey)
 	}
+
 }
 
 func TestEdPrivateKeyToX448(t *testing.T) {
-	// b93a28627cfa29fedb03c21aac0faa1ea0ba84c10cefa07c938f2e0adbf996f02c8d00e39695dfb6a0636c8bcb21645b06a869dfbbb489ef00
-	edKey := PrivateKey{
-		0xb9, 0x3a, 0x28, 0x62, 0x7c, 0xfa, 0x29, 0xfe,
-		0xdb, 0x03, 0xc2, 0x1a, 0xac, 0x0f, 0xaa, 0x1e,
-		0xa0, 0xba, 0x84, 0xc1, 0x0c, 0xef, 0xa0, 0x7c,
-		0x93, 0x8f, 0x2e, 0x0a, 0xdb, 0xf9, 0x96, 0xf0,
-		0x2c, 0x8d, 0x00, 0xe3, 0x96, 0x95, 0xdf, 0xb6,
-		0xa0, 0x63, 0x6c, 0x8b, 0xcb, 0x21, 0x64, 0x5b,
-		0x06, 0xa8, 0x69, 0xdf, 0xbb, 0xb4, 0x89, 0xef,
-		0x00,
-	}
 
-	// 74a4d56b9ca4bb819778d5b089ef89428bbe768825c83264e97cfba7c0a5f3c33d6ac807e3a568d72a605283f89b8afa52b06323704d9278,
-	x448Key := [56]byte{
-		0x74, 0xa4, 0xd5, 0x6b, 0x9c, 0xa4, 0xbb, 0x81,
-		0x97, 0x78, 0xd5, 0xb0, 0x89, 0xef, 0x89, 0x42,
-		0x8b, 0xbe, 0x76, 0x88, 0x25, 0xc8, 0x32, 0x64,
-		0xe9, 0x7c, 0xfb, 0xa7, 0xc0, 0xa5, 0xf3, 0xc3,
-		0x3d, 0x6a, 0xc8, 0x07, 0xe3, 0xa5, 0x68, 0xd7,
-		0x2a, 0x60, 0x52, 0x83, 0xf8, 0x9b, 0x8a, 0xfa,
-		0x52, 0xb0, 0x63, 0x23, 0x70, 0x4d, 0x92, 0x78,
-	}
-	generatedKey := EdPrivateKeyToX448(edKey)
+	p1, _ := hex.DecodeString("b93a28627cfa29fedb03c21aac0faa1ea0ba84c10cefa07c938f2e0adbf996f02c8d00e39695dfb6a0636c8bcb21645b06a869dfbbb489ef00")
+	var priv PrivateKey
+	copy(priv[:], p1[:])
 
+	p2, _ := hex.DecodeString("74a4d56b9ca4bb819778d5b089ef89428bbe768825c83264e97cfba7c0a5f3c33d6ac807e3a568d72a605283f89b8afa52b06323704d9278")
+	var x448Key [56]byte
+	copy(x448Key[:], p2[:])
+
+	generatedKey := EdPrivateKeyToX448(priv)
+	
 	if bytes.Compare(x448Key[:], generatedKey[:]) != 0 {
-		t.Errorf("converted x448 key must be %x, but it is %x", generatedKey, x448Key)
+		t.Errorf("x448 key must be: %x\n But it is: %x", x448Key, generatedKey)
 	}
 }
 
-func TestDeriveSecret(t *testing.T) {
-	priv := PrivateKey{
-		0x1c, 0xcc, 0x9e, 0x4f, 0x16, 0x22, 0x21, 0xab, 
-		0x07, 0xb6, 0x13, 0xcb, 0x55, 0x70, 0xa7, 0x9e, 
-		0xae, 0x68, 0xaf, 0x18, 0xcb, 0xf9, 0x3c, 0xf9, 
-		0xf7, 0xea, 0x29, 0xbd, 0xf4, 0x1b, 0x20, 0x93, 
-		0x9e, 0x7f, 0xa3, 0xd9, 0x87, 0x99, 0xef, 0xff, 
-		0xc0, 0x55, 0xfe, 0x7e, 0x17, 0x1e, 0x5a, 0x11, 
-		0xe7, 0xf2, 0x06, 0x14, 0xd9, 0x4c, 0xc7, 0x1b, 
-		0x60,
-	}
+func TestEd448DeriveSecret(t *testing.T) {
 
-	pub := PublicKey{
-		0x1e, 0xb0, 0x39, 0x16, 0xcf, 0xaf, 0x7a, 0x7f, 
-		0x63, 0x63, 0x72, 0xda, 0x50, 0x0a, 0x16, 0xcf, 
-		0x47, 0x60, 0x16, 0x2c, 0x84, 0xea, 0xb3, 0xb5, 
-		0x27, 0xf4, 0x5f, 0xdb, 0xf7, 0x8f, 0x1b, 0x42, 
-		0xdc, 0x84, 0xc6, 0x35, 0x46, 0x0d, 0xf4, 0xd0, 
-		0x34, 0xc1, 0x40, 0x18, 0x2c, 0xcf, 0xf1, 0x44, 
-		0xc9, 0xcb, 0xfc, 0x03, 0x4d, 0x64, 0xbb, 0xa3, 
-		0x80,
-	}
+	priv1, _ := Ed448GenerateKey(crand.Reader)
+	priv2, _ := Ed448GenerateKey(crand.Reader)
+	public1 := Ed448DerivePublicKey(priv1)
+	public2 := Ed448DerivePublicKey(priv2)
 
-	secret1 := [56]byte{
-		0x21, 0xdf, 0x34, 0xe5, 0x26, 0x38, 0x13, 0x75, 
-		0x9b, 0xd7, 0x62, 0xcc, 0xb2, 0xbc, 0x14, 0xf2, 
-		0xd6, 0xea, 0x30, 0x73, 0xfd, 0x01, 0xa5, 0xae, 
-		0xcd, 0x7c, 0x57, 0x47, 0x2f, 0xea, 0xf9, 0x1b, 
-		0x85, 0xa2, 0xa7, 0xf4, 0x59, 0x59, 0x47, 0xf3, 
-		0x5d, 0xdd, 0x0a, 0xa8, 0xc9, 0xe7, 0x68, 0x10, 
-		0x8b, 0x30, 0xe5, 0xfe, 0x42, 0x37, 0x2e, 0x46,
-	}
+	generatedKey1 := Ed448DeriveSecret(public1, priv2)
+	generatedKey2 := Ed448DeriveSecret(public2, priv1)
 
-	secret2 := Ed448DeriveSecret(pub, priv)
-
-	if bytes.Compare(secret1[:], secret2[:]) != 0 {
-		t.Errorf("secret must be %x, but it is %x", secret1, secret2)
+	if bytes.Compare(generatedKey1[:], generatedKey2[:]) != 0 {
+		t.Errorf("Secret, generated from %x\nand from %x\nmust be the same, but they are not", priv1, priv2)
 	}
 }
 
+func TestPrivateToSecret(t *testing.T) {
 
-func TestDerivePublicKey(t *testing.T) {
-	priv := PrivateKey{
-		0xf8, 0xe0, 0x72, 0xd7, 0xe1, 0x7c, 0x63, 0x4b, 
-		0x27, 0xf2, 0x5d, 0x85, 0xb0, 0x3a, 0xb6, 0x0e, 
-		0xa5, 0xa5, 0xe8, 0xd4, 0x7c, 0x0f, 0xfb, 0xf8, 
-		0x84, 0x31, 0xed, 0xdc, 0x90, 0x9b, 0x47, 0x2b, 
-		0x7f, 0xa0, 0x60, 0xf3, 0xfb, 0x39, 0x11, 0xea, 
-		0xed, 0x8e, 0xb5, 0xa6, 0xad, 0x5a, 0x65, 0xe9, 
-		0x9a, 0xe7, 0x7c, 0x0f, 0x31, 0xfb, 0x14, 0xd3, 
-		0xfc,
+	p, _ := hex.DecodeString("a8ea212cc24ae0fd029a97b64be540885af0e1b7dc9faf4a591742850c4377f857ae9a8f87df1de98e397a5867dd6f20211ef3f234ae71bc56")
+	var privKey PrivateKey
+	copy(privKey[:], p[:])
+
+	p, _ = hex.DecodeString("1413821ed67083c855c6db4405dd4fa5fdec39e1c761be1415623c1c202c5cb5176e578830372b7e07eb1ef9cf71b19518815c4da0fd2d3594")
+	var secretKey PrivateKey
+	copy(secretKey[:], p[:])
+
+	generatedKey := PrivateToSecret(privKey)
+	
+	if bytes.Compare(secretKey[:], generatedKey[:]) != 0 {
+		t.Errorf("Secret key must be: %x\n But it is: %x", secretKey, generatedKey)
 	}
 
-	pub1 := PublicKey{
-		0xc0, 0xd6, 0xf5, 0xf3, 0x27, 0xb1, 0x29, 0x0d, 
-		0xbd, 0x0f, 0xc5, 0xb2, 0x4a, 0xe8, 0x5c, 0x16, 
-		0x64, 0x2b, 0x60, 0xe5, 0x6a, 0x3a, 0x38, 0xd9, 
-		0xfc, 0xf3, 0x41, 0x77, 0xd0, 0xe5, 0x09, 0x7f, 
-		0x99, 0x7f, 0x1b, 0x87, 0x23, 0x3d, 0x95, 0x7b, 
-		0xe6, 0xa3, 0x09, 0x1d, 0xd8, 0x17, 0xbf, 0x9f, 
-		0x93, 0xdc, 0x9f, 0x7f, 0x49, 0x7e, 0x99, 0x68, 
-		0x80,
-	}
-	pub2 := Ed448DerivePublicKey(priv)
-
-	if bytes.Compare(pub1[:], pub2[:]) != 0 {
-		t.Errorf("public key must be %x, but it is %x", pub1, pub2)
-	}
 }
 
-func TestSignMessage(t *testing.T) {
-	priv := PrivateKey{
-		0x85, 0xd5, 0x31, 0x7e, 0x80, 0xb2, 0x1c, 0xc6, 
-		0xac, 0x1d, 0x25, 0x08, 0xee, 0x40, 0xb1, 0xa3, 
-		0x4a, 0x0f, 0x9f, 0x2c, 0xba, 0xc2, 0xde, 0xfd, 
-		0x76, 0x52, 0x45, 0xb7, 0x4d, 0xbc, 0x82, 0xf4, 
-		0x09, 0x8e, 0xae, 0x8b, 0xa0, 0x86, 0x71, 0x6c, 
-		0xe5, 0x95, 0x5f, 0xf0, 0x49, 0x93, 0x9d, 0x8c, 
-		0xfa, 0xeb, 0xa7, 0x61, 0xd3, 0x5e, 0xc3, 0x00, 
-		0x8d, 
+func TestSecretToPublic(t *testing.T) {
+
+	p, _ := hex.DecodeString("1413821ed67083c855c6db4405dd4fa5fdec39e1c761be1415623c1c202c5cb5176e578830372b7e07eb1ef9cf71b19518815c4da0fd2d3594")
+	var secretKey PrivateKey
+	copy(secretKey[:], p[:])
+
+	p, _ = hex.DecodeString("b615e57dd4d15c3ed1323725c0ba8b1d7f6e740d08e0e29c6d3ff564c896c0c3dd28a9bb5065e06725c8f9e3f7c2c6bbad4900b7447ecf9880")
+	var public PublicKey
+	copy(public[:], p[:])
+
+	generatedKey := SecretToPublic(secretKey)
+	
+	if bytes.Compare(public[:], generatedKey[:]) != 0 {
+		t.Errorf("Public key must be: %x\n But it is: %x", public, generatedKey)
 	}
 
-	sig1 := [114]byte {
-		0x4b, 0x3b, 0x09, 0x49, 0xcf, 0x44, 0x39, 0x4b, 
-		0x01, 0xda, 0x93, 0x25, 0xaa, 0x25, 0xf1, 0xea, 
-		0xd0, 0x1a, 0x85, 0x38, 0x30, 0xb7, 0x62, 0xa3, 
-		0xf0, 0x95, 0xbe, 0x86, 0x0d, 0xe6, 0xfd, 0xeb, 
-		0x57, 0x41, 0x97, 0x61, 0x67, 0x19, 0x0e, 0x4c, 
-		0x49, 0xd8, 0x3a, 0x55, 0x0b, 0x82, 0xda, 0xd9, 
-		0x8b, 0x18, 0xda, 0x61, 0xc0, 0x44, 0x88, 0xb1, 
-		0x80, 0x9f, 0x20, 0x6d, 0xcb, 0x36, 0xf3, 0xcb, 
-		0x3f, 0x23, 0x9f, 0x2d, 0xf2, 0x17, 0x34, 0xba, 
-		0xed, 0x91, 0x26, 0x9b, 0x40, 0x39, 0xa5, 0x47, 
-		0xfe, 0x77, 0x70, 0x61, 0x93, 0x6c, 0xab, 0xd9, 
-		0x46, 0x89, 0xe7, 0x6e, 0x52, 0x32, 0x4b, 0x45, 
-		0xc1, 0x66, 0x67, 0x50, 0xae, 0x18, 0x09, 0xad, 
-		0x80, 0xf9, 0xf5, 0x33, 0x0a, 0x86, 0x4c, 0x02, 
-		0x05, 0x00,
-	}
-	pub := Ed448DerivePublicKey(priv)
-	sig2 := Ed448Sign(priv, pub, []byte{1}, []byte{}, false)
 
+}
+
+func TestPrivateToPublic(t *testing.T) {
+
+	p, _ := hex.DecodeString("a8ea212cc24ae0fd029a97b64be540885af0e1b7dc9faf4a591742850c4377f857ae9a8f87df1de98e397a5867dd6f20211ef3f234ae71bc56")
+	var privKey PrivateKey
+	copy(privKey[:], p[:])
+
+	p, _ = hex.DecodeString("b615e57dd4d15c3ed1323725c0ba8b1d7f6e740d08e0e29c6d3ff564c896c0c3dd28a9bb5065e06725c8f9e3f7c2c6bbad4900b7447ecf9880")
+	var public PublicKey
+	copy(public[:], p[:])
+
+	generatedKey := PrivateToPublic(privKey)
+	
+	if bytes.Compare(public[:], generatedKey[:]) != 0 {
+		t.Errorf("Public key must be: %x\n But it is: %x", public, generatedKey)
+	}
+
+}
+
+
+func TestRandomKey(t *testing.T) {
+	privKey, _ := Ed448GenerateKey(crand.Reader)
+	privKey[56] &= 0x7f
+	secretKey := PrivateToSecret(privKey)
+	public1 := SecretToPublic(secretKey)
+	public2 := PrivateToPublic(privKey)
+
+	if bytes.Compare(public1[:], public2[:]) != 0 {
+		t.Errorf("Public from secret: %x\nDoesnt matches public from private: %x", public1, public2)
+	}
+
+}
+
+func TestEd448DerivePublicKey(t *testing.T) {
+
+	p, _ := hex.DecodeString("582f73eb3d951ef93a8c392c7b113ad85c0f60a744c95c47370d4d593593edc0d745eb24fa2130f51fd5b1e6b2363a5405bf1e074ecbf4382d")
+	var privKey PrivateKey
+	copy(privKey[:], p[:])
+
+	p, _ = hex.DecodeString("4e6ef3aa2a74ce85c9c75de379c72abbce30601db4f66af1535d00190fa5de83af3831fa32e37c59e14a25788e56140896fb59b494e4fdca80")
+	var public PublicKey
+	copy(public[:], p[:])
+
+	generatedKey := Ed448DerivePublicKey(privKey)
+	
+	if bytes.Compare(public[:], generatedKey[:]) != 0 {
+		t.Errorf("Public key must be: %x\n But it is: %x", public, generatedKey)
+	}
+
+	p, _ = hex.DecodeString("59fc82f514f3fc8d02d987e52a03cdcae81a257bed6ec9b668bf6acd8fe9e7d27cbcc4d8f463d917642d30e7ca44c3521370f78790b3b561dd")
+	copy(privKey[:], p[:])
+	p, _ = hex.DecodeString("3cba3b2560c2779170ce5947f55bf73b93a1dd51d99b0b483ed0cfb5a9bb8409830c0f96068c799dbc6a28ca6bc1aad95d0387c36a731d7800")
+	copy(public[:], p[:])
+	generatedKey = Ed448DerivePublicKey(privKey)
+
+	if bytes.Compare(public[:], generatedKey[:]) != 0 {
+		t.Errorf("Public key must be: %x\n But it is: %x", public, generatedKey)
+	}
+
+
+}
+
+func TestSignWithPrivate(t *testing.T) {
+
+	p, _ := hex.DecodeString("64c2754ee8f55f285d1c6efac34345c78da28df5c31d9ae3748417e0754903004eca31389e978df148e3941de8d4c3585b6dd3669903f00bb5")
+	var priv PrivateKey
+	copy(priv[:], p[:])
+
+	s, _ := hex.DecodeString("d3ffe2cffeba84f631c9e4f452c7f27023b48e679f30ad9f43b4ef0483670e25842efdd6a20ad74f2c08351e37857763c0e1b787a7a02c5c00708263b206ab852e865676b3b8ad2c86794cd2831b54064cda39e2703a4c172a1debf051e01ae981c58a577731127f2bfb7aaa3f9242572400")
+	var sig1 [114]byte
+	copy(sig1[:], s[:])
+
+
+	fox := []byte("The quick brown fox jumps over the lazy dog")
+	pub := PrivateToPublic(priv)
+	sig2 := SignWithPrivate(priv, pub, fox, []byte{}, false)
+	
 	if bytes.Compare(sig1[:], sig2[:]) != 0 {
-		t.Errorf("signature must be %x, but it is %x", sig1, sig2)
+		t.Errorf("Signature must be %x, but it is %x", sig1, sig2)
 	}
 }
 
+
+func TestSignSecretAndNonce(t *testing.T) {
+
+	p, _ := hex.DecodeString("26ad14d91ef8f1e5bbf5a1a7e44a9532e4854f1e1346761ee9b4ed1ed103e5e05c87fd9ecd788bc879a7433a7115255b7aad667fe84ee35c28")
+	var secret PrivateKey
+	copy(secret[:], p[:])
+
+	p, _ = hex.DecodeString("66dd9754284a1b7d77c1c43bfdfe38a116bd143e7c901b8e8e4561a7ee0a401dd5120fa2b77e2a6bda3a68d5a47e34fd29cf14ce3489067602")
+	var nonce PrivateKey
+	copy(nonce[:], p[:])
+
+	s, _ := hex.DecodeString("71e4ae51aa4d1f59f10efaaca743ca557079c2de1d298375d80eac8c53d29567add49f6296206f6c0d56ad3cd3f34b3644b1b01361900bea803aae2018aea2db72a2c5557a207ba17b8316335817b4a9474def73b3ea0ddaaae593e76596fbeac45c8ef04df3bb23dc809d2b7db49dbf0a00")
+	var sig1 [114]byte
+	copy(sig1[:], s[:])
+
+
+	fox := []byte("The quick brown fox jumps over the lazy dog")
+	pub := SecretToPublic(secret)
+	sig2 := SignSecretAndNonce(secret, nonce, pub, fox)
+	
+	if bytes.Compare(sig1[:], sig2[:]) != 0 {
+		t.Errorf("Signature must be %x, but it is %x", sig1, sig2)
+	}
+}
+
+
+
+func TestEd448Sign(t *testing.T) {
+	
+	p, _ := hex.DecodeString("e959068474bc720bf3a94c7a524750f0d4fe68a4828137e58d48303af1fa929a6c50f87d0cab27fc557aa1a3190cfad0abbca2a2e5d7da272d")
+	var priv PrivateKey
+	copy(priv[:], p[:])
+
+	s, _ := hex.DecodeString("92a7e08f86b25f288eb0308f3fb780950ab77c333d5d1b91b6de40a199fc028fe66a001dc09341905a58f8c3d4a959ee5d416735f59d91640095dd83e70b6bc05fa6a26b32c00be454bfb87285417554183c2da64bbbad77b746bd86299fd4188578bc9aa321a8291c5d2281029ca24e2d00")
+	var sig1 [114]byte
+	copy(sig1[:], s[:])
+
+	fox := []byte("The quick brown fox jumps over the lazy dog")
+	pub := Ed448DerivePublicKey(priv)
+	sig2 := Ed448Sign(priv, pub, fox, []byte{}, false)
+	
+	if bytes.Compare(sig1[:], sig2[:]) != 0 {
+		t.Errorf("Signature must be %x, but it is %x", sig1, sig2)
+	}
+
+	p, _ = hex.DecodeString("1edc2069350104b5594c602f7967c4b1580f2a757fc9a2745f621868cd333c245ec3c775d730d3c01a2e18f3e5d0b5e767ed3ec77e69732781")
+	copy(priv[:], p[:])
+	s, _ = hex.DecodeString("789dd9e1a4471c30cfef1da68076542e6918676424593936dbeb282f5929dcfa3437aef85fd890999ea7a1b16a2c8c3a8cf330c58768789b006b183034ec43acab783039d53fe46f6c39ab29f988a43371d07fe7746a2fd45c660f2a8c441446b8f1cdbfc0787e4cfe69280e5cd7b92d0400")
+	copy(sig1[:], s[:])
+
+	pub = Ed448DerivePublicKey(priv)
+	sig2 = Ed448Sign(priv, pub, fox, []byte{}, false)
+	
+	if bytes.Compare(sig1[:], sig2[:]) != 0 {
+		t.Errorf("Signature must be %x, but it is %x", sig1, sig2)
+	}
+
+
+}
 
 func TestSignVerify(t *testing.T) {
-	priv := PrivateKey{
-		0xb9, 0x3a, 0x28, 0x62, 0x7c, 0xfa, 0x29, 0xfe,
-		0xdb, 0x03, 0xc2, 0x1a, 0xac, 0x0f, 0xaa, 0x1e,
-		0xa0, 0xba, 0x84, 0xc1, 0x0c, 0xef, 0xa0, 0x7c,
-		0x93, 0x8f, 0x2e, 0x0a, 0xdb, 0xf9, 0x96, 0xf0,
-		0x2c, 0x8d, 0x00, 0xe3, 0x96, 0x95, 0xdf, 0xb6,
-		0xa0, 0x63, 0x6c, 0x8b, 0xcb, 0x21, 0x64, 0x5b,
-		0x06, 0xa8, 0x69, 0xdf, 0xbb, 0xb4, 0x89, 0xef,
-		0x00,
-	}
-	
+
+	p, _ := hex.DecodeString("b93a28627cfa29fedb03c21aac0faa1ea0ba84c10cefa07c938f2e0adbf996f02c8d00e39695dfb6a0636c8bcb21645b06a869dfbbb489ef00")
+	var priv PrivateKey
+	copy(priv[:], p[:])
+
 	pub := Ed448DerivePublicKey(priv)
-
 	sig := Ed448Sign(priv, pub, []byte{1}, []byte{}, false)
-	ver := Ed448Verify(pub, sig[:], []byte{1}, []byte{}, false)
-	if ver != true {
-		t.Errorf("Signature doesn't verify")
-	}
-
-	sig = Ed448Sign(priv, pub, []byte{1}, []byte{}, false)
 	if Ed448Verify(pub, sig[:], []byte{2}, []byte{}, false) {
-		t.Errorf("Signature verifies, but it must not")
+		t.Errorf("wrong signature verification")
 	}
 
 
+	pub = Ed448DerivePublicKey(priv)
+	sig = Ed448Sign(priv, pub, []byte{1}, []byte{}, false)
+	if !Ed448Verify(pub, sig[:], []byte{1}, []byte{}, false) {
+		t.Errorf("Signature must be valid")
+	}
+
+
+}
+
+func TestAddTwoPublic(t *testing.T) {
+
+	p, _ := hex.DecodeString("5475efbfc0fa155f3fd80a8c183260eef996532fd084899e32df9cb8db9eb34410d2ea0d4f8b273fbd79c3276b50b70fea40732ad88f45de00")
+	var pub1 PublicKey
+	copy(pub1[:], p[:])
+
+
+	p, _ = hex.DecodeString("d666091b1b3836d082d349e66521878cf7afc734329d5d132d8ebd06bebf6514aaad5794dbafed3fd6aa1c5d59d5db914e8460041ff3db6280")
+	var pub2 PublicKey
+	copy(pub2[:], p[:])
+
+	p, _ = hex.DecodeString("6af04e1137833cbf82878fcdcd8310851fe582320690990a7497de63389311588d4c0d4d6fce79d07958824e54ef11fabd23b815c81e79ea80")
+	var pub PublicKey
+	copy(pub[:], p[:])
+
+	generatedKey := AddTwoPublic(pub1, pub2)
+	
+	if bytes.Compare(pub[:], generatedKey[:]) != 0 {
+		t.Errorf("Public key must be %x, but it is %x", generatedKey, pub)
+	}
 }
