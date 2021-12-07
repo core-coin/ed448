13c77428dcfc2d3be0dab8784aeff9999573cafd
Arman Mazdaee <arman.maz1373@gmail.com>
2021-12-07 18:08:52 +0330

    update Ed448GenerateKey for HD wallets

--- mobile/goldilocks.go
+++ mobile/goldilocks.go
@@ -1,23 +1,29 @@
 package mobile
 
 import (
-	"bytes"
+	"strconv"
 
 	"github.com/core-coin/ed448"
 )
 
-func Ed448GenerateKey(seed string) (string, error) {
+func Ed448GenerateKey(seed string, index string) (string, error) {
 	sb, err := decodeBytes(seed)
 	if err != nil {
 		return "", err
 	}
 
-	privKey, err := ed448.Ed448GenerateKey(bytes.NewBuffer(sb))
+	iu, err := strconv.ParseUint(index, 10, 32)
 	if err != nil {
 		return "", err
 	}
 
-	return encodeBytes(privKey[:]), nil
+	m := ed448.SeedToExtendedPrivate([]uint8(sb))
+	k1 := ed448.ChildPrivateToPrivate(m, 0x80000000+44)
+	k2 := ed448.ChildPrivateToPrivate(k1, 0x80000000+654)
+	k3 := ed448.ChildPrivateToPrivate(k2, 0x80000000+0)
+	k4 := ed448.ChildPrivateToPrivate(k3, uint32(iu))
+
+	return encodeBytes(k4[57:]), nil
 }
 
 func Ed448DerivePublicKey(privKey string) (string, error) {
